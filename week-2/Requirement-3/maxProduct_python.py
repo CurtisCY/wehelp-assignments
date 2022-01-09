3eecf4b9e7666cad8c89fa8bb8fd77abe754fcfb
CurtisCY <63384830+CurtisCY@users.noreply.github.com>
2022-01-09 20:57:30 +0800

    Add files via upload

--- week-2/Requirement-3/maxProduct_python.py
+++ week-2/Requirement-3/maxProduct_python.py
@@ -12,10 +12,17 @@ def maxProduct(nums):
                 nums[j+1] = nums[j]
                 nums[j] = valueTemp
 
-    print("排序結果", nums)
-    print("兩數相乘最大值", nums[-2]*nums[-1])
+    if (nums[-2]*nums[-1]>nums[0]*nums[1]):
+        print("排序結果", nums)
+        print("兩數相乘最大值", nums[-2]*nums[-1])
+    else:
+        print("排序結果", nums)
+        print("兩數相乘最大值", nums[0]*nums[1])
 
-maxProduct([5,20,2,6])
-maxProduct([10, -20, 0, 3])
-maxProduct([-1, 2])
-maxProduct([-1, 0, 2])
+
+maxProduct([5,20,2,6]) #Get 120
+maxProduct([10, -20, 0, 3]) #Get 30
+maxProduct([-1, 2]) # Get -2
+maxProduct([-1, 0, 2]) # Get 0
+maxProduct([5,-1,-2,0]) # Get 2
+maxProduct([-7,-1,-2,-10]) # Get 70
